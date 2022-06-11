1cfdfef88892ce96daefe64954170368f5d34016
Rob Kritzinger <kritz.rob@gmail.com>
2022-06-11 10:15:55 +0200

    add user service

---
+++ src/services/user.service.js
@@ -0,0 +1,15 @@
+const fakeUser = (token) =>
+  new Promise((resolve) => {
+    setTimeout(() => resolve(
+      {
+        "_id": "628f8ebbd644b7d4a928e2ca",
+        "name": "John",
+        "surname": "Guy",
+        "username": "John.Guy",
+        "email": "test1@test.com",
+        "bio": "Hi, I'm John, and I like cheese"
+      }
+    ), 250);
+  });
+
+export { fakeUser };
