004ab2bab0b329d78b1992251b2b72952f7e8eff
Rob Kritzinger <kritz.rob@gmail.com>
2022-06-18 21:58:26 +0200

    work on post compose

--- src/components/post-composer/post-composer.js
+++ src/components/post-composer/post-composer.js
@@ -1,13 +1,36 @@
+import axios from 'axios';
 import { useState } from 'react';
 import './post-composer.css'
+import config from '../../config'
+import { useAuth } from '../../providers/auth.provider'
 
 function PostCompose() {
 
+    const { token } = useAuth();
     const [text, setText] = useState('');
 
-    const createPost = () => {
-        //create post
+    const onCreate = () => {
+
         console.log('creating post: ', text);
+
+        const createPost = async () => {
+            try {
+                const result = await axios({
+                    method: "POST",
+                    url: config.api_url + 'posts/create',
+                    headers: config.headers(token),
+                    data: {
+                        text: text
+                    }
+                })
+
+
+            } catch (e) {
+                console.log(e);
+            }
+        }
+
+        createPost();
     }
 
     const handleTextChange = (e) => {
@@ -15,10 +38,10 @@ function PostCompose() {
     }
 
     return (
-        <div className='post-compose'> 
+        <div className='post-compose'>
             <textarea className='compose-body' placeholder='Post something...' onChange={handleTextChange} />
             <div className='compose-operations'>
-                <button onClick={createPost} className='submit-post'>Post</button>
+                <button onClick={onCreate} className='submit-post'>Post</button>
             </div>
         </div>
     )
