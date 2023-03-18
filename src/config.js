2c09836d9076d53eac91ddb75ce046904f13d72f
Rob <kritz.rob@gmail.com>
2023-03-18 10:04:34 +0200

    update media config

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 
 export default {
     api_url: process.env.REACT_APP_API_URL,
-    media_url: (id) => process.env.REACT_APP_MEDIA_URL.replace('<id>', id),
+    media_url: (id) => 'https://drive.google.com/uc?id=<id>&export=view'.replace('<id>', id),
     headers: (token) => { 
         return { 
             'Authorization': `Bearer ${token}`,
