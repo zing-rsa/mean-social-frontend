f96fd5d2928201f85c32e18a33c7e527532632d2
Rob Kritzinger <kritz.rob@gmail.com>
2023-03-24 21:15:20 +0200

    about

--- src/views/about/about.js
+++ src/views/about/about.js
@@ -15,9 +15,9 @@ function About() {
                     </div>
                 </div>
                 <div className="about-body">
-                Mern Social is a code project that aims to simulate a social media similar to Twitter, where users can create accounts, log in, and post short messages + media to share with others. Users can also follow and unfollow each other, as well as like and reply to posts.
-                <br /><br />The primary purpose of this project was to explore the JavaScript(MERN) ecosystem and improve my skills in React and API design. Where I could, I have minimized use of libraries to gain a more vanilla understanding of the technology. For example, I was curious how production applications implement their login/refresh mechanism so the mechanism used in this site is written completely by me.
-                <br /><br />It's been a fun opportunity to expand my skill-set and build my experience in developing web applications. I plan to leave the project as an open sandbox with the idea to explore and implement ideas that spike my curiosity in the future.
+                Mern Social is a code project that aims to simulate a social media similar to Twitter, where users can create accounts, log in, and post short messages + media to share with others. Other interactions include mentions/tags, follows, likes and comments, which will all generate notifications.  
+                <br /><br />The primary purpose of the project was to explore the JavaScript(MERN) ecosystem and improve my skills in React and API design. Where I could, I have minimized use of libraries to gain a more vanilla understanding of the technology. For example I was curious how production applications implement their login/refresh mechanism so the mechanism used in this site is written completely by me.
+                <br /><br />I plan to leave the project as an open sandbox with the idea to explore and implement ideas that spike my curiosity in the future. Contributions welcome.
                 <br /><br />Thanks for checking it out.
                 </div>
                 <div className='about-links'>
