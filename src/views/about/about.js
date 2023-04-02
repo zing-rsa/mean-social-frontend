f967a561275553aa3846d76604c8a41da23b6208
Rob <kritz.rob@gmail.com>
2023-04-02 16:42:00 +0200

    about

--- src/views/about/about.js
+++ src/views/about/about.js
@@ -15,9 +15,9 @@ function About() {
                     </div>
                 </div>
                 <div className="about-body">
-                Mern Social is a code project that aims to simulate a social media similar to Twitter, where users can create accounts, log in, and post short messages + media to share with others. Other interactions include mentions/tags, follows, likes and comments, which will all generate notifications.  
-                <br /><br />The primary purpose of the project was to explore the JavaScript(MERN) ecosystem and improve my skills in React and API design. Where I could, I have minimized use of libraries to gain a more vanilla understanding of the technology. For example I was curious how production applications implement their login/refresh mechanism so the mechanism used in this site is written completely by me.
-                <br /><br />I plan to leave the project as an open sandbox with the idea to explore and implement ideas that spike my curiosity in the future. Contributions welcome.
+                Mern Social is a code project that simulates a social media similar to Twitter, where users can create accounts, log in, and post short messages + media to share with others. Other interactions include mentions/tags, follows, likes and comments, which will all generate notifications.  
+                <br /><br />The purpose of the project was to explore the JavaScript(MERN) ecosystem and improve my skills in React and API design. Where I could, I have minimized use of libraries to get a more vanilla understanding of the technology. For example, I was curious how production applications implement their login/refresh mechanism so the one used in this site is written completely by me.
+                <br /><br />I plan to leave the project as an open sandbox with the aim to explore and implement ideas that spike my curiosity in the future. Contributions welcome.
                 <br /><br />Thanks for checking it out.
                 </div>
                 <div className='about-links'>
