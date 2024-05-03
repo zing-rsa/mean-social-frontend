73958cde0ec70d01f30534d36df05686b0198f61
Rob Kritzinger <kritz.rob@gmail.com>
2024-05-03 15:02:19 +0200

    chore(): update google drive url

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 
 export default {
     api_url: process.env.REACT_APP_API_URL,
-    media_url: (id) => 'https://drive.google.com/uc?id=<id>&export=view'.replace('<id>', id),
+    media_url: (id) => 'https://drive.usercontent.google.com/download?id=<id>&export=view'.replace('<id>', id),
     headers: (token) => { 
         return { 
             'Authorization': `Bearer ${token}`,
